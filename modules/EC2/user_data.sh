fedf31f724565ee85d2873aa01937e6232afe05e
Nasrullah <nasrullahzafar.dae@gmail.com>
2024-01-31 23:58:14 +0500

    set env

--- modules/EC2/user_data.sh
+++ modules/EC2/user_data.sh
@@ -8,8 +8,8 @@ sudo apt install docker.io -y
 
 sudo docker login
 
-sudo docker login -u nasrullahzafardae333 -p ${var.DOCKER_PASSWORD}
+sudo docker login -u ${var.DOCKER_ACCOUNT_ID} -p ${var.DOCKER_PASSWORD}
 
-sudo docker pull nasrullahzafardae333/python_apps:latest 
+sudo docker pull ${var.DOCKER_ACCOUNT_ID}/python_apps:latest 
 
-sudo docker run -d -p 80:80 --name "container_python_app" nasrullahzafardae333/python_apps
+sudo docker run -d -p 80:80 --name "container_python_app" ${var.DOCKER_ACCOUNT_ID}/python_apps
